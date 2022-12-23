402d7003d194a776ffcc8ce19713f5bdf68779ad
Michele Santagiuliana <micheles@apadmi.com>
2022-12-23 16:14:27 +0000

    Made the list interactive

--- src/main/kotlin/VideoList.kt
+++ src/main/kotlin/VideoList.kt
@@ -1,17 +1,26 @@
+import kotlinx.browser.window
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML
 import react.dom.html.ReactHTML.p
 import react.key
+import react.useState
 
 external interface VideoListProps : Props {
     var videos: List<Video>
 }
 
 val VideoList = FC<VideoListProps> { props ->
+    var selectedVideo: Video? by useState(null)
     for (video in props.videos) {
         p {
             key = video.id.toString()
+            onClick = {
+                selectedVideo = video
+            }
+            if (video == selectedVideo) {
+                +"▶ "
+            }
             +"${video.speaker}: ${video.title}"
         }
     }
