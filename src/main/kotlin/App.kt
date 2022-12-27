322f2109ab85a43a517c5e9af33c0fb3905cc081
Michele Santagiuliana <micheles@apadmi.com>
2022-12-27 10:36:46 +0000

    Added video lists to the application state

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -11,6 +11,18 @@ import react.useState
 
 val App = FC<Props> {
     var currentVideo: Video? by useState(null)
+    var unwatchedVideos: List<Video> by useState(
+        listOf(
+            Video(1, "Opening Keynote", "Andrey Breslav", "https://youtu.be/PsaFVLr8t4E"),
+            Video(2, "Dissecting the stdlib", "Huyen Tue Dao", "https://youtu.be/Fzt_9I733Yg"),
+            Video(3, "Kotlin and Spring Boot", "Nicolas Frankel", "https://youtu.be/pSiZVAeReeg")
+        )
+    )
+    var watchedVideos: List<Video> by useState(
+        listOf(
+            Video(4, "Creating Internal DSLs in Kotlin", "Venkat Subramaniam", "https://youtu.be/JzTeAM8N1-o")
+        )
+    )
 
     h1 {
         +"KotlinConf Explorer"
@@ -41,6 +53,16 @@ val App = FC<Props> {
         currentVideo?.let { curr ->
             VideoPlayer {
                 video = curr
+                unwatchedVideo = curr in unwatchedVideos
+                onWatchedButtonPressed = {
+                    if (video in unwatchedVideos) {
+                        unwatchedVideos = unwatchedVideos - video
+                        watchedVideos = watchedVideos + video
+                    } else {
+                        watchedVideos = watchedVideos - video
+                        unwatchedVideos = unwatchedVideos + video
+                    }
+                }
             }
         }
         img {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,13 +13,3 @@ data class Video(
     val speaker: String,
     val videoUrl: String
 )
-
-val unwatchedVideos = listOf(
-    Video(1, "Opening Keynote", "Andrey Breslav", "https://youtu.be/PsaFVLr8t4E"),
-    Video(2, "Dissecting the stdlib", "Huyen Tue Dao", "https://youtu.be/Fzt_9I733Yg"),
-    Video(3, "Kotlin and Spring Boot", "Nicolas Frankel", "https://youtu.be/pSiZVAeReeg")
-)
-
-val watchedVideos = listOf(
-    Video(4, "Creating Internal DSLs in Kotlin", "Venkat Subramaniam", "https://youtu.be/JzTeAM8N1-o")
-)
